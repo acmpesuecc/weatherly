e55b00b517db79f0962982e8b81d6df78c66b7af
avinash-vk <avinash2000vk@gmail.com>
2020-10-23 18:36:06 +0530

    added api key for openweather

--- src/api/Weather.js
+++ src/api/Weather.js
@@ -8,7 +8,8 @@
 
 // TODO: API base url for openweathermap.org
 const BASE_URL = "";
-const API_KEY = process.env.OPEN_WEATHER_MAP_API;
+
+const API_KEY = process.env.OPEN_WEATHER_MAP_API || "13fe6c2601bd7c8549c14a35c7238aef";
 /*
     TODO: Query the endpoint for the json format of weather data for the location.
     parameters : location, and anything else the endpoint requires.
