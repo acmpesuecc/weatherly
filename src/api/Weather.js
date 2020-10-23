26de794b5fba073da13a5286c7f5d08990a332df
avinash-vk <avinash2000vk@gmail.com>
2020-10-23 10:51:27 +0530

    added api endpoint

---
+++ src/api/Weather.js
@@ -0,0 +1,23 @@
+/*
+    Functions to call the https://openweathermap.org api to get the weather data of that location.
+*/
+
+/*
+    Note: Make sure to make interactions using the JSON format. 
+*/
+
+// TODO: API base url for openweathermap.org
+const BASE_URL = "";
+
+/*
+    TODO: Query the endpoint for the json format of weather data for the location.
+    parameters : location, and anything else the endpoint requires.
+    returns : processed json weather data
+*/
+const GET_WEATHER = () => {
+    return null;
+};
+
+export default {
+   GET_WEATHER
+}
