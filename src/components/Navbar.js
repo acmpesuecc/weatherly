e8927b93a01da2e4bff0a1cfdf5922753c16c9b0
Shreyas Panale <shreya.panale@gmail.com>
2020-10-24 22:04:49 +0530

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,80 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles';
+import {AppBar, Toolbar, IconButton, Typography} from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+const styles = makeStyles((theme) => ({
+    wrapper: {
+        [theme.breakpoints.down('sm')]: {
+          display: 'none',
+        },
+        margin: '10px',
+        paddingLeft: '16px',
+        right: 0,
+        position: 'relative',
+        width: '100%',
+        background: 'transparent',
+      },
+    listContainer: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+      },
+      listMenus: {
+        float: 'right',
+      },
+      listItems: {
+        display: 'inline-block',
+        textAlign: 'center',
+        padding: '6px 8px',
+        color: 'white',
+        textDecoration: 'none',
+      },
+    navbar: {
+      backgroundColor: '#000000',
+    },
+    appTitle: {
+      fontSize: '1.5em',
+    },
+  }));
+
+const Navbar = (props) => {
+    const classes = styles();
+    const logo =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png';
     return (
-        <h1>
-            Weatherly
-        </h1>
-    )
-}
-
+      <AppBar position="sticky" className={classes.navbar}>
+        <Toolbar>
+        <IconButton
+            color="inherit"
+            aria-label="Menu"
+        >
+        <img src={logo} className="img-xxs" height={20} />
+        </IconButton>
+        <Typography
+            variant="title"
+            color="inherit"
+            className={classes.appTitle}
+        >
+        Weatherly
+        </Typography>
+        <div className={classes.wrapper}>
+        <ul className={classes.listContainer}>
+        <li className={classes.listMenus}>
+            <Link to="/landing" className={classes.listItems}>
+              Landing
+            </Link>
+          </li>
+        <li className={classes.listMenus}>
+            <Link to="/dashboard" className={classes.listItems}>
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+      </div>
+        </Toolbar>
+      </AppBar>
+    );
+  };
 export default Navbar;
