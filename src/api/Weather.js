00d7d1f09968971c48f0cedff44e0339fcd2e519
Lara Aasem <lara-aasem@hotmail.com>
2020-10-29 01:24:36 +0200

    Add JSDocs

--- src/api/Weather.js
+++ src/api/Weather.js
@@ -12,6 +12,14 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const API_KEY = process.env.OPEN_WEATHER_MAP_API || "13fe6c2601bd7c8549c14a35c7238aef";
 
+/**
+ *
+ * @param {Object} location
+ * @param {string} location.cityName
+ * @param {string} [location.stateCode='']
+ * @param {string} [location.countryCode='']
+ * @returns {JSON}
+ */
 const GET_WEATHER = async ({ cityName, stateCode = '', countryCode = '' }) => {
     const url = `${BASE_URL}/weather?appid=${API_KEY}&q=${cityName},${stateCode},${countryCode}`
     let response = await axios.get(url)
