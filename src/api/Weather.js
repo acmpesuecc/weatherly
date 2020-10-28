19f9967f67b68be961a41368ccc6c50fb66feb17
Lara Aasem <lara-aasem@hotmail.com>
2020-10-29 01:14:08 +0200

    Implement GET_WEATHER

--- src/api/Weather.js
+++ src/api/Weather.js
@@ -6,19 +6,17 @@
     Note: Make sure to make interactions using the JSON format.
 */
 
-// TODO: API base url for openweathermap.org
-const BASE_URL = "";
+const request = require('request-promise-native')
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const API_KEY = process.env.OPEN_WEATHER_MAP_API || "13fe6c2601bd7c8549c14a35c7238aef";
-/*
-    TODO: Query the endpoint for the json format of weather data for the location.
-    parameters : location, and anything else the endpoint requires.
-    returns : processed json weather data
-*/
-const GET_WEATHER = () => {
-    return null;
+
+const GET_WEATHER = async ({ cityName, stateCode = '', countryCode = '' }) => {
+    const url = `${BASE_URL}/weather?appid=${API_KEY}&q=${cityName},${stateCode},${countryCode}`
+    let response = await request.get(url)
+    if (response) { response = JSON.parse(response) }
+    return response
 };
 
-export default {
-   GET_WEATHER
-}
+module.exports = GET_WEATHER
